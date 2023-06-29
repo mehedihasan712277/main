68103565464f12d420192cdc43545c3e7738bf58
Mehedi <mehedihasan712277@gmail.com>
2023-06-29 13:58:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+    return (
+        <>
+            <div className="container">
+                <div className="title">React</div>
+                <div className="subtitle">A javascript library</div>
+                <p className="desc">Lorem ipsum dolor sit amet</p>
+                <button>Click</button>
+            </div>
+        </>
+    )
+}
+
+export default App
